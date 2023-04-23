2498a7fdd510da3ff3a661ca573872f6b934d389
unknown <chongjiayi2001@gmail.com>
2023-04-23 23:46:34 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/uum/my_negara2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.uum.my_negara2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,154 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:audioplayers/audioplayers.dart';
+import 'package:quickalert/quickalert.dart';
+
+// ignore: camel_case_types
+class homePage extends StatefulWidget {
+  const homePage({super.key});
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+// ignore: camel_case_types
+class _homePageState extends State<homePage> {
+  TextEditingController input = TextEditingController();
+  String name = '';
+  String currencyCode = '';
+  String currencyName = '';
+  String capital = '';
+  String resultiso2 = '';
+  String description = "";
+  ImageProvider flag = const NetworkImage('');
+  final player = AudioPlayer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 234, 234, 234),
+      body: SingleChildScrollView(
+        child: Center(
+            child: Column(
+            children: [
+            Image.asset('assets/images/logo.png', scale: 3),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(50, 5, 50, 5),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: input,
+                    decoration: InputDecoration(
+                        hintText: 'Enter Country',
+                        hintStyle:
+                            const TextStyle(color: Colors.grey, fontSize: 20),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(80),
+                        )),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                    child: ElevatedButton(
+                      onPressed: search,
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 16, 23, 53),
+                          padding: const EdgeInsets.all(5),
+                          fixedSize: const Size(300, 50),
+                          elevation: 15,
+                          shadowColor: Colors.black,
+                          side: const BorderSide(style: BorderStyle.solid)),
+                      child: const Text(
+                        'Search',
+                        style: TextStyle(
+                            fontFamily: 'San-serif',
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                  height: 300,
+                  width: 400,
+                  padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color.fromARGB(255, 192, 192, 192)),
+                    color: const Color.fromARGB(255, 239, 239, 239)
+                  ),
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(
+                      image:
+                      flag,
+                      errorBuilder: (BuildContext context, Object exception,
+                      StackTrace? stackTrace) {
+                      return const Text('');
+                      },
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+                      child: Text(
+                        description,
+                        textAlign: TextAlign.justify,
+                        style: const TextStyle(
+                          fontFamily: 'San-serif',
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600
+                        ),
+                      ),
+                    ),         
+                  ],
+                )),
+                ],
+              ),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+
+  Future<void> search() async {
+    String country = input.text;
+    String apiid = "VXbDdlnA9+4cMTVekLcDcQ==AmRnYGozAF91LyWC";
+    Uri url = Uri.parse('https://api.api-ninjas.com/v1/country?name=$country');
+    var response = await http.get(url, headers: {"X-Api-Key": apiid});
+    if (response.statusCode == 200) {
+      var jsonData = response.body;
+      List parsedJson = json.decode(jsonData);
+      List searchedName = [];
+      List searchedCapital = [];
+      List searchedCurrencyCode = [];
+      List searchedCurrencyName = [];
+      List searchediso2 = [];
+      if (parsedJson.isNotEmpty) {
+        searchedName.add(parsedJson[0]['name']);
+        searchedCurrencyCode.add(parsedJson[0]['currency']['code']);
+        searchedCurrencyName.add(parsedJson[0]['currency']['name']);
+        searchedCapital.add(parsedJson[0]['capital']);
+        searchediso2.add(parsedJson[0]['iso2']);
+        name = searchedName.join('');
+        currencyCode = searchedCurrencyCode.join('');
+        currencyName = searchedCurrencyName.join('');
+        capital = searchedCapital.join('');
+        resultiso2 = searchediso2.join('');
+        description =
+            "This country is $name with $capital as capital. Currency used is $currencyCode, $currencyName.";
+        flag = NetworkImage('https://flagsapi.com/$resultiso2/shiny/64.png');
+        player.play(AssetSource('audios/correct_tone.mp3'));
+        // ignore: use_build_context_synchronously
+        QuickAlert.show(context: context, text: 'Country Found!', type: QuickAlertType.success);
+        setState(() {});
+      } else {
+        player.play(AssetSource('audios/error_tone.mp3'));
+        // ignore: use_build_context_synchronously
+        QuickAlert.show(context: context, text: 'Country Not Found! Please re-enter.', type: QuickAlertType.error);
+      }
+    }
+  }
+}
